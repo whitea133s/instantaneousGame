603fa95225dd4476f67e0db13780f8341c726c07
大久保です <y2ta8bsc@icloud.com>
2025-06-09 15:38:18 +0900

    コメント追加

--- game.js
+++ game.js
@@ -1,10 +1,10 @@
-const delay = Math.random() * 8000 + 3000;
+const delay = Math.random() * 8000 + 3000; //３s～10s秒
 
 function switchLump() {
   const showlump = document.getElementById("lump");
   showlump.classList.remove("hidden");
 }
-console.log(Math.floor(delay / 1000));
+console.log(Math.floor(delay / 1000) + "秒後に表示");
 setTimeout(() => {
   switchLump();
 }, delay);
