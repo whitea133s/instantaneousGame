93a8c0e07c1843dbdea61a05620716bece16622b
whitea133s <y2ta8bsc@icloud.com>
2025-06-10 21:11:17 +0900

    スコアボード

--- game.js
+++ game.js
@@ -1,5 +1,9 @@
 const showlump = document.getElementById("lump");
 let timeoutId = null;
+let startTime = null;
+let endTime = null;
+let score = document.querySelector(".score");
+score.textContent = null;
 
 document.getElementById("start").addEventListener("click", function () {
   const delay = Math.random() * 8000 + 3000; //3s～10s秒
@@ -10,6 +14,7 @@ document.getElementById("start").addEventListener("click", function () {
   console.log(Math.floor(delay / 1000) + "秒後に表示");
   timeoutId = setTimeout(() => {
     showlump.classList.remove("hidden");
+    startTime = Date.now();
   }, delay);
 });
 
@@ -18,6 +23,13 @@ document.getElementById("reset").addEventListener("click", function () {
   showlump.classList.add("hidden");
   clearTimeout(timeoutId);
   timeoutId = null;
+  score.textContent = null;
 });
 
-document.getElementById("judg").addEventListener("click", function () {});
+document.getElementById("judg").addEventListener("click", function () {
+  endTime = Date.now();
+  score.textContent =
+    "ランプが点灯してから" +
+    (endTime - startTime).toString().substring(0, 4) / 1000 +
+    "秒かかりました。";
+});
