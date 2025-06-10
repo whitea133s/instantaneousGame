aeda0a79ca225f3160ffa1c71bbff8512b50937f
大久保です <y2ta8bsc@icloud.com>
2025-06-10 10:41:24 +0900

    リセットボタンの実装

--- game.js
+++ game.js
@@ -1,10 +1,20 @@
-const delay = Math.random() * 8000 + 3000; //３s～10s秒
+const showlump = document.getElementById("lump");
+let timeoutId = null;
 
-function switchLump() {
-  const showlump = document.getElementById("lump");
-  showlump.classList.remove("hidden");
-}
-console.log(Math.floor(delay / 1000) + "秒後に表示");
-setTimeout(() => {
-  switchLump();
-}, delay);
+document.getElementById("start").addEventListener("click", function () {
+  const delay = Math.random() * 8000 + 3000; //3s～10s秒
+  if (timeoutId !== null) {
+    clearTimeout(timeoutId);
+  }
+  console.log(Math.floor(delay / 1000) + "秒後に表示");
+  timeoutId = setTimeout(() => {
+    showlump.classList.remove("hidden");
+  }, delay);
+});
+
+document.getElementById("reset").addEventListener("click", function () {
+  console.log("ゲームが中断されました");
+  showlump.classList.add("hidden");
+  clearTimeout(timeoutId);
+  timeoutId = null;
+});
